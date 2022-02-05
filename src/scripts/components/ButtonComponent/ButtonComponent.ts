8f96fbd294bbde8f79e223b646b287cbedecb8f6
AndreyDrobysh <andrey.drobysh.04@gmail.com>
2022-02-05 15:07:14 +0300

    feat: add components

---
+++ src/scripts/components/ButtonComponent/ButtonComponent.ts
@@ -0,0 +1,15 @@
+import BaseComponent from '../BaseComponent/BaseComponent';
+
+
+export default class ButtonComponent extends BaseComponent<HTMLButtonElement> {
+  constructor(classes:string [] = [], text?: string) {
+    super('button', classes);
+    if (text) {
+      this.element.innerHTML = text;
+    }
+  }
+
+  onClick(cb: (e?:Event) => void) {
+    this.element.addEventListener('click', cb);
+  }
+}
